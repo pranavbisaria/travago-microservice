4883c8f719001ec4f4992b5fbc4c0bf7df0c1ea3
Pranav Bisaria <pranavbisariya29@gmail.com>
2024-02-22 23:14:01 +0530

    init: rating service

---
+++ rating-service/src/main/java/com/travago/rating/TravagoRatingApplication.java
@@ -0,0 +1,13 @@
+package com.travago.rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravagoRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravagoRatingApplication.class, args);
+	}
+
+}
