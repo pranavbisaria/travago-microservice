a5bbe48d0bbdecbe33d137f7446d2dce11ddc400
Pranav Bisaria <pranavbisariya29@gmail.com>
2024-02-23 01:21:40 +0530

    add: getby hotel Id and User Id

--- rating-service/src/main/java/com/travago/platform/rating/repository/HotelRatingRepository.java
+++ rating-service/src/main/java/com/travago/platform/rating/repository/HotelRatingRepository.java
@@ -3,7 +3,11 @@ package com.travago.platform.rating.repository;
 import com.travago.platform.rating.entity.HotelRating;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface HotelRatingRepository extends JpaRepository<HotelRating, String> {
+    List<HotelRating> findByHotelId(String hotelId);
+
+    List<HotelRating> findByUserId(String userId);
 }
---
+++ rating-service/src/main/java/com/travago/platform/rating/service/impl/RatingServiceImpl.java
@@ -0,0 +1,35 @@
+package com.travago.platform.rating.service.impl;
+
+import com.travago.platform.rating.entity.HotelRating;
+import com.travago.platform.rating.repository.HotelRatingRepository;
+import com.travago.platform.rating.service.RatingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RatingServiceImpl implements RatingService {
+    private final HotelRatingRepository hotelRatingRepository;
+
+    @Override
+    public HotelRating create(HotelRating hotelRating) {
+        return this.hotelRatingRepository.save(hotelRating);
+    }
+
+    @Override
+    public List<HotelRating> getRatings() {
+        return this.hotelRatingRepository.findAll();
+    }
+
+    @Override
+    public List<HotelRating> getRatingByUserId(String userId) {
+        return this.hotelRatingRepository.findByUserId(userId);
+    }
+
+    @Override
+    public List<HotelRating> getRatingByHotelId(String hotelId) {
+        return this.hotelRatingRepository.findByHotelId(hotelId);
+    }
+}
