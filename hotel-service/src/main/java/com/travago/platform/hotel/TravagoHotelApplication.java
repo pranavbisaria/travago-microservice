86bbc06d7db05a0bcac639bcfd8dbfa26c19d106
Pranav Bisaria <pranavbisariya29@gmail.com>
2024-02-22 23:56:53 +0530

    fix: package name issue

--- hotel-service/src/main/java/com/travago/platform/hotel/TravagoHotelApplication.java
+++ hotel-service/src/main/java/com/travago/platform/hotel/TravagoHotelApplication.java
@@ -1,4 +1,4 @@
-package com.travago.hotel;
+package com.travago.platform.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
