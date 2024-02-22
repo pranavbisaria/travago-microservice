d0eb295be626b8d3977f040bfef6c0024f5eff8d
Pranav Bisaria <pranavbisariya29@gmail.com>
2024-02-22 23:43:42 +0530

    init: hotel service module

---
+++ hotel-service/src/main/java/com/travago/platform/hotel/TravagoHotelApplication.java
@@ -0,0 +1,13 @@
+package com.travago.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravagoHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravagoHotelApplication.class, args);
+	}
+
+}
