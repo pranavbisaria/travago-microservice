c37690b8b7a1e0599ed5489427a2c52f8ad1a926
Pranav Bisaria <pranavbisariya29@gmail.com>
2024-02-23 00:29:03 +0530

    add: hotel service methods

---
+++ hotel-service/src/main/java/com/travago/platform/hotel/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.travago.platform.hotel.exception;
+
+import com.travago.platform.user.model.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Response> handlerResourceNotFoundException(ResourceNotFoundException ex) {
+        Response response = Response.builder()
+                .message(ex.getMessage())
+                .status(NOT_FOUND).build();
+        return new ResponseEntity<>(response, NOT_FOUND);
+    }
+}
